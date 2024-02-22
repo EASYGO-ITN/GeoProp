3fce050610768b153ff2279d0336fb122019f5fa
tlmerbecks <tristanleonard.merbecks@polimi.it>
2024-02-22 11:00:50 +0100

    bug fix: calculations options were not being set correctly

--- GeoPropV2/model/engine.py
+++ GeoPropV2/model/engine.py
@@ -1,11 +1,11 @@
 from . import models
 
 
-def get_model(model, *args, **kwargs):
+def get_model(model, options):
 
     if model not in models:
         string_models = ','.join(mod for mod in models)
         msg = "The specified model \"{}\" is not recognised. The following models are available: " + string_models + "."
         raise ValueError(msg)
 
-    return models[model](args, kwargs)
+    return models[model](model, options=options)
